c56921a3e95492f6c59966cb96c580223e0e413d
Mike Brown <devilmn073@gmail.com>
2015-06-06 17:45:07 -0400

    made migrations safe

--- config/models.js
+++ config/models.js
@@ -29,6 +29,8 @@ module.exports.models = {
   * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-   migrate: 'alter'
+   migrate: 'safe'
+   //migrate: 'alter'
+   
 
 };
