fc195bb1b07191baa1921bb7fad1b7356351d154
Mason Vines <mason.vines@gmail.com>
2015-06-06 16:10:00 -0400

    Showing all markers, not getting from database yet.

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -7,13 +7,9 @@
 
 module.exports = {
 	  index: function (req, res) {
+		var suggestions = SuggestionFinder.findSuggestions(0, 0);
     	return res.view('homepage', {
-			    "id": "0001",
-			    "latitude": 38.3018,
-			    "longitude": -77.4708,
-			    "description": "This is the first suggestion, you guys should be able to see it",
-			    "createdAt": "2015-06-06T17:53:15.448Z",
-			    "updatedAt": "2015-06-06T17:53:15.448Z"
+			'suggestions': suggestions
 		});
   	}
 };
